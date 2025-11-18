7b32f640c49df1052b694e52a3744ff95923b717
gopakumar7199 <gzmonster79@gmail.com>
2025-11-18 15:15:26 +0530

    Updated multienvironment deployment

--- app/src/server.js
+++ app/src/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'local';
 
 app.get('/', (req, res) => {
-  res.send(`Hello everyone and welcome to my web application running in  ${ENV} environment.`);
+  res.send(`Hello guys! This is a test for Multi-environment deployment working! ENV: ${ENV}`);
 });
 
 app.listen(PORT, () => {
