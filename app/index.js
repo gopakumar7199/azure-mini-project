80c375efb56cdc05ab9230b06fd40b7a138b27db
gopakumar7199 <gzmonster79@gmail.com>
2025-11-22 19:09:42 +0530

    Add files via upload

---
+++ app/index.js
@@ -0,0 +1,36 @@
+// index.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handle WebSocket connections
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (msg) => {
+    console.log('Message received:', msg);
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the server
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
